646db6b7a243a8bccf4642e7a056cba451104168
feifeifanye <feifeifanye@hotmail.com>
2018-04-23 19:45:38 +0800

    fix bugs

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,6 +7,6 @@ require_once 'index.php';
 $isDevMode = true;
 $config    = Setup::createAnnotationMetadataConfiguration(array(APP_PATH . 'src/Entity/'), $isDevMode);
 
-$conn = 'configs/db.php';
+$conn = require 'configs/db.php';
 
 $entityManager = EntityManager::create($conn, $config);
